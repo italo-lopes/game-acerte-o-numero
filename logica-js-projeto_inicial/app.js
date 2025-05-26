55c9fad6d3f0bb32a0474af11cf42c4a50f7b8d2
Italo de Assis Silva Lopes <italo-lopes-v1@outlook.com>
2025-05-26 19:30:15 -0300

    primeiro comandos

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,10 @@
 //alert("Hello, World");
+
+// let varieavel em js (usado pragurdar variavel)
+const numeroSecreto = 29;
+let numero = prompt("Digite um numero");
+console.log(valor);
+
+if(numero == numeroSecreto){
+    console.log(numero);
+}
