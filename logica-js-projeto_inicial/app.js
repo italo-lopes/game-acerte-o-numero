d105ce600643be5916ec11bf0778131d2e462fb2
Italo de Assis Silva Lopes <italo-lopes-v1@outlook.com>
2025-05-26 22:17:19 -0300

    desafio 1

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,19 @@
 //alert("Hello, World");
-
+// prompt("input") recebe valor na caixa de dialogo
 // let varieavel em js (usado pragurdar variavel)
-const numeroSecreto = 29;
-let numero = prompt("Digite um numero");
-console.log(valor);
-
-if(numero == numeroSecreto){
-    console.log(numero);
-}
+let numeroSecreto = null;
+let chute = null;
+const buttonComandao1 = document.getElementById("comando-1");
+alert("Hello, World");
+buttonComandao1.addEventListener("click", ()=>{
+     numeroSecreto = parseInt((Math.random() * 9)+1) ;
+     console.log(numeroSecreto);
+     chute = prompt("Digite um numero de 1 a 10");
+    if(chute == numeroSecreto){
+        alert(`O numero secreto é ${numeroSecreto}`);
+        console.log("Voce acertou o numero: "+ numeroSecreto);
+    }else{
+        alert("Error - Tente Novamente \n \n O número secretos era: "+ numeroSecreto);
+        console.log("Não acertou o número secreto era: "+numeroSecreto);
+    }
+})
