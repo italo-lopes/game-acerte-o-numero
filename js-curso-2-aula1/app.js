74af6f183d9618950d7a716042f82b4389f127e2
Italo de Assis Silva Lopes <italo-lopes-v1@outlook.com>
2025-07-08 09:32:05 -0300

    capitulo 2

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,31 +1,52 @@
 const chuteButton = document.getElementById("chute");
 const reiniciarButton = document.getElementById("reiniciar");
-let tentativa = 0;
+let tentativa = null;
+let valor = null;
+reiniciar();
+
 function showText(tag,texto){
     const elemento = document.querySelector(tag);
     elemento.innerHTML = texto;
 }
-
 function randomNumber(){
     return parseInt((Math.random() * 10) + 1); 
 }
-
-showText("h1","Jogo do Número Secreto");
-showText("p","Escolha o número de 1 a 10");
-
-const valor = randomNumber();
-console.log("Valor random : "+valor);
+function inicializarTexto(){
+    showText("h1","Jogo do Número Secreto");
+    showText("p","Escolha o número de 1 a 10");
+}
 
 chuteButton.addEventListener("click",()=>{
-    const value = document.querySelector('input').value;
+    const chute = document.querySelector('input').value;
     tentativa++;
-      console.log(tentativa);
-    console.log(valor == value);
-    if(valor == value){
-        tentativa=0;
+    if(valor == chute){
+        let tentativaString = tentativa == 1 ? "Tentativa" : "Tentativas";
+        let mensagem = `Voce descobriu o número secreto com ${tentativa} ${tentativaString}`;
+        showText("h1","Acertou");
+        showText("p",mensagem);
+        chuteButton.setAttribute("disabled",true);
+        reiniciarButton.removeAttribute("disabled");
+    }else{
+        showText("h1","Jogo do Número Secreto");
+        if(chute > valor){
+            showText("p","O número secreto é menor");
+        }else{
+            showText("p","O número secreto é maior");
+        }
     }
+    limparCampo();
 });
-
 reiniciarButton.addEventListener("click", ()=>{
-    console.log("Reiniciado");
+    reiniciar();
 });
+function limparCampo(){
+    document.querySelector('input').value = "";
+}
+function reiniciar(){
+    valor = randomNumber();
+    tentativa=0;
+    limparCampo();
+    inicializarTexto();
+    reiniciarButton.setAttribute("disabled", true);
+    chuteButton.removeAttribute("disabled");
+}
